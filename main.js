61f2f24652f0ee609164b35d298c5ec1bbef0e59
Brooke T. Hedrick <brooke.t.hedrick@gmail.com>
2016-07-28 01:17:34 +0000

    Added copying var/array/object examples to main.js

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+// http://stackoverflow.com/questions/10729276/how-can-i-get-the-full-object-in-node-js-console-log-rather-than-object
+var util = require('util');
+
 console.log("Hello main world")
 
 var MyValue = 10;
@@ -21,3 +24,56 @@ if (typeof unintendedGlobal == 'undefined') {
 } else {
   console.log("HAS unintendedGlobal");
 }
+
+var myGlobalVariable = 10;
+var myGlobalArray = [100];
+var myGlobalObject = {item:200};
+
+var TestVariable = myGlobalVariable;
+
+console.log('TestVariable = ' + TestVariable);
+console.log('myGlobalVariable = ' + myGlobalVariable);
+
+myGlobalVariable++;
+
+console.log('TestVariable = ' + TestVariable);
+console.log('myGlobalVariable = ' + myGlobalVariable);
+
+
+
+
+
+
+TestVariable = myGlobalArray;
+
+console.log('TestVariable = ' + TestVariable);
+console.log('myGlobalArray = ' + myGlobalArray);
+
+myGlobalArray[0]++;
+
+console.log('TestVariable = ' + TestVariable);
+console.log('myGlobalArray = ' + myGlobalArray);
+
+TestVariable = myGlobalArray.slice();
+
+myGlobalArray[0]++;
+
+console.log('TestVariable = ' + TestVariable);
+console.log('myGlobalArray = ' + myGlobalArray);
+
+
+
+
+
+
+
+
+TestVariable = myGlobalObject;
+
+console.log('TestVariable = ' + util.inspect(TestVariable, {showHidden: false, depth: null}));
+console.log('myGlobalObject = ' + util.inspect(myGlobalObject, {showHidden: false, depth: null}));
+
+myGlobalObject.item++;
+
+console.log('TestVariable = ' + util.inspect(TestVariable, {showHidden: false, depth: null}));
+console.log('myGlobalObject = ' + util.inspect(myGlobalObject, {showHidden: false, depth: null}));
